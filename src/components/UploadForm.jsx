697c0eabef160deba59b824798963164d3678911
Harison <you@example.com>
2026-05-28 01:35:35 -0300

    fix(api): add dependecy 'axios' on UploadForm, to call python search api

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom"; // Importado para fazer a navegação
 import client from "../api/client";
